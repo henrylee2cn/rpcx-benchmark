ea403e54d1d404cf3b0654bee038912d4f8ea99d
henrylee2cn <henrylee_cn@foxmail.com>
2017-11-18 23:11:34 +0800

    Add teleport

---
+++ teleport/tp_client.go
@@ -0,0 +1,175 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"net/http"
+	_ "net/http/pprof"
+	"reflect"
+	"sync"
+	"sync/atomic"
+	"time"
+
+	tp "github.com/henrylee2cn/teleport"
+	"github.com/montanaflynn/stats"
+)
+
+var concurrency = flag.Int("c", 1, "concurrency")
+var total = flag.Int("n", 1, "total requests for all clients")
+var host = flag.String("s", "127.0.0.1:8972", "server ip and port")
+var debugAddr = flag.String("d", "127.0.0.1:9982", "server ip and port")
+var cpuprofile = flag.String("cpuprofile", "", "write cpu profile to file")
+
+func main() {
+	flag.Parse()
+
+	go func() {
+		log.Println(http.ListenAndServe(*debugAddr, nil))
+	}()
+
+	n := *concurrency
+	m := *total / n
+
+	log.Printf("concurrency: %d\nrequests per client: %d\n\n", n, m)
+
+	serviceMethod := "/hello/say"
+
+	args := prepareArgs()
+
+	b := make([]byte, 1024*1024)
+	i, _ := args.MarshalTo(b)
+	log.Printf("message size: %d bytes\n\n", i)
+
+	var wg sync.WaitGroup
+	wg.Add(n * m)
+
+	log.Printf("sent total %d messages, %d message per client", n*m, m)
+
+	var startWg sync.WaitGroup
+	startWg.Add(n)
+
+	var trans uint64
+	var transOK uint64
+
+	d := make([][]int64, n, n)
+
+	tp.SetRawlogLevel("error")
+
+	var peer = tp.NewPeer(&tp.PeerConfig{
+		DefaultReadTimeout:   time.Minute * 5,
+		DefaultWriteTimeout:  time.Second * 5,
+		TlsCertFile:          "",
+		TlsKeyFile:           "",
+		SlowCometDuration:    time.Millisecond * 500,
+		DefaultHeaderCodec:   "protobuf",
+		DefaultBodyCodec:     "protobuf",
+		DefaultBodyGzipLevel: 0,
+		PrintBody:            false,
+	})
+	defer peer.Close()
+
+	//it contains warmup time but we can ignore it
+	totalT := time.Now().UnixNano()
+	for i := 0; i < n; i++ {
+		dt := make([]int64, 0, m)
+		d = append(d, dt)
+
+		go func(i int) {
+			defer func() {
+				if r := recover(); r != nil {
+					log.Print("Recovered in f", r)
+				}
+			}()
+
+			sess, err := peer.Dial(*host)
+			if err != nil {
+				startWg.Done()
+				panic(err)
+			}
+			defer sess.Close()
+
+			startWg.Done()
+			startWg.Wait()
+			var (
+				reply interface{}
+				xerr  tp.Xerror
+			)
+			for j := 0; j < m; j++ {
+				t := time.Now().UnixNano()
+				reply, xerr = sess.Pull(serviceMethod, args, new(BenchmarkMessage)).Result()
+				d[i] = append(d[i], time.Now().UnixNano()-t)
+				if xerr != nil {
+					log.Print(xerr.Error())
+				} else if reply.(*BenchmarkMessage).Field1 == "OK" {
+					atomic.AddUint64(&transOK, 1)
+				}
+				atomic.AddUint64(&trans, 1)
+				wg.Done()
+			}
+		}(i)
+	}
+
+	wg.Wait()
+
+	totalT = time.Now().UnixNano() - totalT
+	totalT = totalT / 1000000
+	log.Printf("took %d ms for %d requests\n", totalT, n*m)
+
+	totalD := make([]int64, 0, n*m)
+	for _, k := range d {
+		totalD = append(totalD, k...)
+	}
+	totalD2 := make([]float64, 0, n*m)
+	for _, k := range totalD {
+		totalD2 = append(totalD2, float64(k))
+	}
+
+	mean, _ := stats.Mean(totalD2)
+	median, _ := stats.Median(totalD2)
+	max, _ := stats.Max(totalD2)
+	min, _ := stats.Min(totalD2)
+	p99, _ := stats.Percentile(totalD2, 99.9)
+
+	log.Printf("sent     requests    : %d\n", n*m)
+	log.Printf("received requests    : %d\n", atomic.LoadUint64(&trans))
+	log.Printf("received requests_OK : %d\n", atomic.LoadUint64(&transOK))
+	log.Printf("throughput  (TPS)    : %d\n", int64(n*m)*1000/totalT)
+	log.Printf("mean: %.f ns, median: %.f ns, max: %.f ns, min: %.f ns, p99.9: %.f ns\n", mean, median, max, min, p99)
+	log.Printf("mean: %d ms, median: %d ms, max: %d ms, min: %d ms, p99: %d ms\n", int64(mean/1000000), int64(median/1000000), int64(max/1000000), int64(min/1000000), int64(p99/1000000))
+
+}
+
+func prepareArgs() *BenchmarkMessage {
+	b := true
+	var i int32 = 100000
+	var s = "许多往事在眼前一幕一幕，变的那麼模糊"
+
+	var args BenchmarkMessage
+
+	v := reflect.ValueOf(&args).Elem()
+	num := v.NumField()
+	for k := 0; k < num; k++ {
+		field := v.Field(k)
+		if field.Type().Kind() == reflect.Ptr {
+			switch v.Field(k).Type().Elem().Kind() {
+			case reflect.Int, reflect.Int32, reflect.Int64:
+				field.Set(reflect.ValueOf(&i))
+			case reflect.Bool:
+				field.Set(reflect.ValueOf(&b))
+			case reflect.String:
+				field.Set(reflect.ValueOf(&s))
+			}
+		} else {
+			switch field.Kind() {
+			case reflect.Int, reflect.Int32, reflect.Int64:
+				field.SetInt(100000)
+			case reflect.Bool:
+				field.SetBool(true)
+			case reflect.String:
+				field.SetString(s)
+			}
+		}
+
+	}
+	return &args
+}
---
+++ teleport/tp_server.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"net/http"
+	_ "net/http/pprof"
+	"time"
+
+	tp "github.com/henrylee2cn/teleport"
+)
+
+type Hello struct {
+	tp.PullCtx
+}
+
+func (h *Hello) Say(args *BenchmarkMessage) (*BenchmarkMessage, tp.Xerror) {
+	args.Field1 = "OK"
+	args.Field2 = 100
+	return args, nil
+}
+
+var (
+	host       = flag.String("s", "127.0.0.1:8972", "listened ip and port")
+	cpuprofile = flag.String("cpuprofile", "", "write cpu profile to file")
+	debugAddr  = flag.String("d", "127.0.0.1:9981", "server ip and port")
+)
+
+func main() {
+	flag.Parse()
+
+	go func() {
+		log.Println(http.ListenAndServe(*debugAddr, nil))
+	}()
+
+	tp.SetRawlogLevel("error")
+
+	var peer = tp.NewPeer(&tp.PeerConfig{
+		DefaultReadTimeout:   time.Minute * 5,
+		DefaultWriteTimeout:  time.Second * 5,
+		TlsCertFile:          "",
+		TlsKeyFile:           "",
+		SlowCometDuration:    time.Millisecond * 500,
+		DefaultHeaderCodec:   "protobuf",
+		DefaultBodyCodec:     "protobuf",
+		DefaultBodyGzipLevel: 0,
+		PrintBody:            false,
+		ListenAddrs:          []string{*host},
+	})
+	defer peer.Close()
+
+	peer.PullRouter.Reg(new(Hello))
+	peer.Listen()
+}
